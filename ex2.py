71623893c57b3dc5fd3ba98733980bd5925eaf9f
Sara-Maude <sara-maude.laliberte@polymtl.ca>
2024-09-11 17:55:04 -0400

    exercice 2 et 4

--- ex2.py
+++ ex2.py
@@ -1,4 +1,13 @@
 # TODO: Créer un script pour calculer les ressources nécessaires pour assainir la Seine.
 # TODO: Importer les modules nécessaires.
+import math
+water_quantity = input("Quelle quantité d'eau faut-il assainir ? ")
+filtre = math.ceil(float(water_quantity)/5)
+lampe = math.ceil((float(water_quantity)/5)*3)
+chlore = float(water_quantity)/10
 
-water_quantity = ...
+answer = f"""Voici les éléments requis pour assainir {water_quantity}L d'eau:\n
+        \t- Filtre(s) : {filtre}\n
+        \t- Lampe(s) UV : {lampe}\n
+        \t- Chlore : {chlore}kg\n"""
+print(answer)
--- ex4.py
+++ ex4.py
@@ -3,4 +3,24 @@
 #        et affiche le résultat au format "XX km".
 #        Assurez une gestion du pourcentage valide au cours de votre programme (% toujours dans [0 ; 100]).
 
-battery_level = ...
+battery_level = float(input("Quel est le pourcentage de batterie du bateau?"))
+if battery_level > 50:
+    distance = 2*(battery_level - 50) + 25*0.5 + 15 + 5*2.5 + 5*6
+
+elif battery_level > 25:
+    distance = 0.5*(battery_level -25) + 15 + 5*2.5 + 5*6
+
+elif battery_level > 10:
+    distance = (battery_level - 10) + 5*2.5 + 5*6
+
+elif battery_level > 5:
+    distance = 2.5*(battery_level - 5) + 5*6
+
+else:
+    distance = 6*battery_level
+
+print(f"La distance possible est de {distance}km")
+
+
+
+
